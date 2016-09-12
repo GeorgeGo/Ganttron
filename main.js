e52f8d7598d2197ac5f1f6047fcdaa3afbfb712a
George Go <george.js.gosieski@gmail.com>
2016-09-12 15:32:51 -0700

    Test of branch change

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let win
 function createWindow() {
     win = new BrowserWindow({width: 800, height:800})
     win.loadURL(`file://${__dirname}/index.html`)
+
     // win.webContents.openDevTools()
     win.on('closed', () => {
         win = null
